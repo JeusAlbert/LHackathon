ae03424d17936474b409dc964d73a826719cdf4f
Ragkhuza <Ragkhuza@gmail.com>
2019-11-09 20:47:15 +0800

    Initial commit

---
+++ public/js/sample.js
@@ -0,0 +1,2 @@
+alert("hello albert");
+alert("located in public/js/sample.js");
---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () { // localhost:8000
+    return view('welcome');
+});
+
+Route::get('/sample', function () { // link to sample html localhost:8000/sample
+    return view('sample'); // @Albert this is the sample html
+});
